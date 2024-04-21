66f561a8810bb248207579ca49ba57f949d36551
Carlos Daniel <carlosporrasayala@gmail.com>
2024-04-21 03:13:28 -0500

    Add a product to the cart through the 'Add to cart' button

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,20 @@ function App() {
   // State
 
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart((prevCart) => [...prevCart, item]);
+    }
+  }
 
   return (
     <>
@@ -17,7 +31,7 @@ function App() {
 
         <div className='row mt-5'>
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
           ))}
         </div>
       </main>
