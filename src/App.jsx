88b208dce9511898f19279d5fd58219560ad2e4a
Carlos Daniel <carlosporrasayala@gmail.com>
2024-04-22 03:02:17 -0500

    Increase product quantity in shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
 
+  const MAX_ITEMS = 5;
+
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
 
@@ -26,9 +28,23 @@ function App() {
     setCart((prevCart) => prevCart.filter((item) => item.id !== id));
   }
 
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
   return (
     <>
-      <Header cart={cart} removeFromCart={removeFromCart} />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+      />
 
       <main className='container-xl mt-5'>
         <h2 className='text-center'>Nuestra Colección</h2>
